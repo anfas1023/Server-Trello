2e9d1ba8ef9feab1372abbd351c8d7acb344b630
Anfas <anfasmuhammed936@gmail.com>
2024-07-11 15:49:57 +0530

    usecase updated

--- src/application/interface/IauthUsecase.ts
+++ src/application/interface/IauthUsecase.ts
@@ -1,15 +1,15 @@
-import { User } from "../../domain/entities/User";
+import { user } from "../../domain/entities/User";
 
 
 export interface IauthUseCase {  
-    userExists(email:string) :Promise<User | null>;
-    registerUser(body:User) : Promise<User>;
-    userLogin(email:string,password:string) :Promise<User | null>;
-    verifyUser(email:string,otp:string) : Promise<User|null>;
-    forgotUsecase(email:string,password:string):Promise<User | null>;
+    userExists(email:string) :Promise<user | null>;
+    registerUser(body:user) : Promise<user>;
+    userLogin(email:string,password:string) :Promise<user | null>;
+    verifyUser(email:string,otp:string) : Promise<user|null>;
+    forgotUsecase(email:string,password:string):Promise<user | null>;
     resendOtp(email:string) : Promise<string>;
-    logedUserData(id:string) :Promise<User | null>;
+    logedUserData(id:string) :Promise<user | null>;
     userVerification(emailToken:string) :Promise<boolean | null>;
     addToSpace(workspaceId:string,email:string,role:string,userName:string,userId:string) :Promise<boolean>;
-    fetchUserUseCase(userId:string) :Promise<User |  null>;
+    fetchUserUseCase(userId:string) :Promise<user |  null>;
 }
